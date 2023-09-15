8448c720698c903499713ef3043e91e2bc4b07aa
notgurev <notgurev@gmail.com>
2023-09-15 13:38:33 +0300

    add comments to utils, etc

--- Core/Src/utils.c
+++ Core/Src/utils.c
@@ -5,12 +5,14 @@ void toggleLED(int code) {
 	HAL_GPIO_TogglePin(GPIOD, code);
 }
 
+// State is GPIO_PIN_SET or GPIO_PIN_RESET
 void setLED(int code, bool state) {
 	HAL_GPIO_WritePin(GPIOD, code, state);
 }
 
+// Resets all three LEDs
 void reset_LEDs() {
-	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, 0);
-	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, 0);
-	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, 0);
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
 }
