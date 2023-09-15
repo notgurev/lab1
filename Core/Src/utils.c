4eed4675eff54d2720d475bbdcfca16f6985bfb4
notgurev <notgurev@gmail.com>
2023-09-15 13:22:56 +0300

    remove is_btn_pressed

--- Core/Src/utils.c
+++ Core/Src/utils.c
@@ -9,19 +9,6 @@ void setLED(int code, bool state) {
 	HAL_GPIO_WritePin(GPIOD, code, state);
 }
 
-bool is_btn_pressed(uint32_t* time) {
-	int last_press_time = *time;
-
-	 if (!HAL_GPIO_ReadPin(GPIOC,GPIO_PIN_15)) { // is pressed
-		 *time = HAL_GetTick();
-		 if (*time - last_press_time > DELAY) {
-			 return true;
-		 }
-	 }
-
-	 return false;
-}
-
 void reset_LEDs() {
 	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, 0);
 	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, 0);
